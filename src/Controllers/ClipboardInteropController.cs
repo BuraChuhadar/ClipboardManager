c4934e3a1cf9c27515812fb17e1278dd549021fd
Bugra Cuhadaroglu <bugracuhadaroglu@hotmail.com>
2016-10-30 02:59:30 -0400

    First project init.

---
+++ src/Controllers/ClipboardInteropController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clipboard.Controllers
+{
+    class ClipboardInteropController
+    {
+        [DllImport("user32.dll")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetClipboardData(uint uFormat, IntPtr data);
+        
+    }
+}
---
+++ src/Views/ClipboardView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Clipboard.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clipboard.Views
+{
+    /// <summary>
+    /// Interaction logic for Clipboard.xaml
+    /// </summary>
+    public partial class ClipboardView : Window
+    {
+        System.Windows.Forms.NotifyIcon clipboardIcon = new System.Windows.Forms.NotifyIcon();
+        public ClipboardView()
+        {
+            clipboardIcon.Visible = true;
+            clipboardIcon.Icon = new System.Drawing.Icon(@"Resources\Icon1.ico");
+            clipboardIcon.ShowBalloonTip(5000, "Title", "Text", System.Windows.Forms.ToolTipIcon.Info);
+            InitializeComponent();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            var hotkeyController = new HotkeyController.HotKey(Key.V, HotkeyController.KeyModifier.Shift | HotkeyController.KeyModifier.Ctrl, OnHotKeyHandler);
+        }
+
+        private void OnHotKeyHandler(HotkeyController.HotKey obj)
+        {
+            this.Show();
+        }
+    }
+}
